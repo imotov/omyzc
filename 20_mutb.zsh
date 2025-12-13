4b1b303a6510db1d43876d03fb840f80d7676ac4
Igor Motov <igor@motovs.org>
2025-12-13 11:25:07 -1000

    Add more mutb utilities

--- 20_mutb.zsh
+++ 20_mutb.zsh
@@ -4,25 +4,25 @@ IMAGE_NAME="mutb:latest"
 OS=$(uname)
 
 if [[ $OS == "Darwin" ]]; then
-    if command -v docker >/dev/null 2>&1; then
-        if docker system info >/dev/null 2>&1; then
-            if [[ -n "$(docker image ls -q "$IMAGE_NAME")" ]]; then
-                mutb() {
-                    docker run --rm \
-                        -v "$PWD":"$PWD" \
-                        -w "$PWD" \
-                        --user "$(id -u):$(id -g)" \
-                        mutb:latest "$@"
-                }
-                exiftool() {
-                    mutb exiftool "$@"
-                }
-                fixexif() {
-                    exiftool "-filemodifydate<datetimeoriginal" `find . -type f -newermt "00:00 today"`
-                }
-            fi
-        fi
-    fi
+    mutb() {
+        docker run --rm \
+            -v "$PWD":"$PWD" \
+            -w "$PWD" \
+            --user "$(id -u):$(id -g)" \
+            mutb:latest "$@"
+    }
+    exiftool() {
+        mutb exiftool "$@"
+    }
+    fixexif() {
+        exiftool "-filemodifydate<datetimeoriginal" `find . -type f -newermt "00:00 today"`
+    }
+    fdupes() {
+        mutb fdupes "$@"
+    }
+    find_dup_footage() {
+        fdupes . -r | grep "^\./Footage" -v ".DS_Store" | sort 
+    }
 fi    
 
 
